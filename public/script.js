d0db9a34812593516a1237e29cc15f22432e2f18
RohitTechGroup70 <151532046+onemanarmyrohit432@users.noreply.github.com>
2024-01-19 17:28:05 +0530

    my-resume

---
+++ public/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".Timer",{
+    strings:["Full Stack Developer","Java Developer","Web Developer"],
+    typeSpeed:100,
+    backSpace:100,
+    backDelay:1000,
+    loop:true
+});
